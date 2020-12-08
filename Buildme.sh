0c13fcededaaefc4099ff3b4505d73daac4aebe6
Jeremy Strader <tempest.nano@gmail.com>
2020-12-07 22:38:22 -0500

    finalize changes fix multibuild

--- Buildme.sh
+++
@@ -1,8 +0,0 @@
-docker build -t jstrader/docker-amd:arm32v7 -f .\Dockerfile.arm32v7 ./
-docker push jstrader/docker-amd:arm32v7
-docker build -t jstrader/docker-amd:arm64v8 -f .\Dockerfile.arm64v8 ./
-docker push jstrader/docker-amd:arm64v8
-docker build -t jstrader/docker-amd:amd64 -f .\Dockerfile ./
-docker push jstrader/docker-amd:amd64
-docker manifest create jstrader/docker-amd:latest --amend jstrader/docker-amd:arm32v7 --amend jstrader/docker-amd:arm64v8 --amend jstrader/docker-amd:amd64
-docker manifest push jstrader/docker-amd:latest
--- buildme.bat
+++
@@ -1,9 +0,0 @@
-
-(
-   start "task1" cmd /C "docker build -t jstrader/docker-amd:arm32v7 -f .\Dockerfile.arm32v7 ./ & docker push jstrader/docker-amd:arm32v7"
-   start "task2" cmd /C "docker build -t jstrader/docker-amd:arm64v8 -f .\Dockerfile.arm64v8 ./ & docker push jstrader/docker-amd:arm64v8"
-   start "task3" cmd /C "docker build -t jstrader/docker-amd:amd64 -f .\Dockerfile ./ & docker push jstrader/docker-amd:amd64"
-) | pause
-
-docker manifest create jstrader/docker-amd:latest --amend jstrader/docker-amd:arm32v7 --amend jstrader/docker-amd:arm64v8 --amend jstrader/docker-amd:amd64
-docker manifest push jstrader/docker-amd:latest
